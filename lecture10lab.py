c6cec85b7d33b0c7d9a3d50b1c44d882951b297e
navinksrk <140748337+navinksrk@users.noreply.github.com>
2024-04-17 07:21:21 -0500

    Update lecture10lab.py

--- lecture10lab.py
+++ lecture10lab.py
@@ -9,10 +9,11 @@ Then, use groupby() to create an object df_color which groups by color.
 """
 EXERCISE 2
 
-The color is graded from D to Z, though it caps out at J in our data.
-D is colorless and diamonds get progressively darker as we go towards J.
-Colorless diamonds are the most desirable of diamonds and should be the most
-expensive: https://www.lumeradiamonds.com/diamond-education/diamond-color
+The color of a diamond is graded from D to Z, though it maxes out at J 
+in our data. D is colorless and diamonds get progressively darker as we 
+go towards J. Colorless diamonds are the most desirable of diamonds 
+and should be the most expensive: 
+https://www.lumeradiamonds.com/diamond-education/diamond-color
 
 Using the method mean() check whether D-color diamonds are in fact
 more expensive than other categories. Do you seen any trend? Is it in
@@ -38,27 +39,27 @@ I    5091.87
 J    5323.82
 Name: price, dtype: float64
 
-In other words - darker diamonds appear to be *more* expensive! In
-contrast to our expectation that colorless diamonds should be more
-expensive
+In other words - darker diamonds appear to be *more* expensive
+on average! This is in contrast to our expectation that darker 
+diamonds should be less expensive
 """
 
 """
 EXERCISE 3
 
 Here's a theory for why that's happening: our results are being
-confounded by weight. Darker diamonds would usually sell for less
-but in our dataset, they are also larger, which pushes up the price
+confounded by weight. Darker diamonds would usually sell for less.
+In our dataset, they are also larger, which pushes up the price.
 
 To check whether this theory is plausible, check how the average 
 weight (in carats) varies across groups. If it appears that darker
-colors are also more larger, that lends support to this theory
+colors are also larger, that lends support to this theory
 """
 
 """
 EXERCISE 4
 
-Short: check if the effect is softened when we "control" for size
+Short: check if the effect is diminished when we "control" for size
 
 Long: 1. Look up the average weight of diamonds in the dataset
 
@@ -68,5 +69,6 @@ if the diamond is smaller than the average weight and 0 otherwise
 3. Create a groupby object using both color AND this indicator
 variable
 
-4. Check the mean price by group in this variable 
+4. Check the mean price by group for this combination of categories, 
+focusing on the price of small diamonds 
 """
